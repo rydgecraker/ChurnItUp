188dbb3e2d1fcafa4069e2a7af2eca383d9810ee
Craker, Rydge <rcraker5@lab-ecb152a-12.cvtc.local>
2018-02-08 15:44:52 -0600

    Initial Commit

---
+++ ChurnItUp_Real/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChurnItUp_Real
+//
+//  Created by Craker, Rydge on 2/8/18.
+//  Copyright © 2018 Craker, Rydge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
