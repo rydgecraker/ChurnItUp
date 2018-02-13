e59df2dd7c9625bbf2bfa9195c53c29212a45e33
Craker, Rydge <rcraker5@lab-ecb152a-12.cvtc.local>
2018-02-13 13:20:26 -0600

    Added second view controller and the start button (not linked to the second view controller yet). Also named the view controllers.

--- ChurnItUp_Real/ViewController.swift
+++ ChurnItUp_Real/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func PressedStartButton(_ sender: Any) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
