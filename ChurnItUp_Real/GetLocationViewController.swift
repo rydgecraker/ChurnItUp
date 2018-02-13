ab647f4f63525dfbfd8316cc7d57f0f1cd0beb74
Craker, Rydge <rcraker5@lab-ecb152a-12.cvtc.local>
2018-02-13 16:25:38 -0600

    Added more lat/long logic

--- ChurnItUp_Real/GetLocationViewController.swift
+++ ChurnItUp_Real/GetLocationViewController.swift
@@ -20,7 +20,7 @@ class GetLocationViewController: UIViewController {
     let oneMeterInLatitudeDegrees = 0.000009
     
     //Because longitude changes with latitude, the distance between longitude lines must be calculated on the spot. with cos(latitudeDegrees) * mileDistance at the equator (69.172 miles between each line)
-    func getOneMeterInLongitudeDegrees(latitudeDegrees: CGFloat) -> CGFloat{
+    func getOneMeterInLongitudeDegrees(latitudeDegrees: Double) -> Double{
         let oneMileDegrees = cos(latitudeDegrees) * 69.172
         return oneMileDegrees / 1069.34
     }
@@ -47,10 +47,31 @@ class GetLocationViewController: UIViewController {
     }
     
     func plopCow(){
-        print("CurrentLocation \(longitude) long and \(latitude) lat")
+        print("CurrentLocation: \(longitude) long and \(latitude) lat")
+        let cowLatChangeInMeters = convert0to60toNeg30to30(Double (arc4random_uniform(60)) + 1.0)
+        let cowLat = getCowLatitude(cowLatChangeInMeters: cowLatChangeInMeters)
+        let cowLonChangeInMeters = convert0to60toNeg30to30(Double (arc4random_uniform(60)) + 1.0)
+        let cowLon = getCowLongitude(cowLonChangeInMeters: cowLonChangeInMeters, cowLat: cowLat)
+        print("Cow is going to be \(cowLonChangeInMeters) meters lat and \(cowLonChangeInMeters) meters lon")
+        print("That means the cow will be \(cowLat) degrees lat and \(cowLon) degrees lon")
     }
     
+    func getCowLatitude(cowLatChangeInMeters: Double) -> Double {
+        let cowLatChangeInDegrees = oneMeterInLatitudeDegrees * cowLatChangeInMeters
+        return latitude + cowLatChangeInDegrees
+    }
     
+    func getCowLongitude(cowLonChangeInMeters: Double, cowLat: Double) -> Double {
+        let cowLonChangeInDegrees = getOneMeterInLongitudeDegrees(latitudeDegrees: cowLat) * cowLonChangeInMeters
+        return longitude * cowLonChangeInDegrees
+    }
+    
+    func convert0to60toNeg30to30(_ number: Double) -> Double {
+        if(number > 30){
+            return number - 61 //Will never return 0. Returns [-30, -1] and [1, 30]
+        }
+        return number
+    }
 
 }
 
