73d19c768c4fbeed0d9406181b20cb6b9d42930a
BenRohland <benrohland@yahoo.com>
2018-02-15 16:21:29 -0600

    Changed the background in main game screne to a SKNode and sized it to the screen, also changed the Churn sprites to size based on the screen size

--- ChurnItUp_Real/MainGameScene.swift
+++ ChurnItUp_Real/MainGameScene.swift
@@ -13,31 +13,37 @@ class MainGameScene: SKScene {
     
     private let ChurnStaffNodeName = "staff"
     private let ChurnBaseNodeName = "base"
+    private let BackgroundNodeName = "background"
     
     
     override init(size: CGSize) {
         super.init(size: size)
         
+        let background = SKSpriteNode()
+        background.position = CGPoint(x: size.width/2.0, y: size.height/2.0)
+        //background.size = CGSize(width: size.width, height: size.height)
+        background.texture = SKTexture(imageNamed: "barn.png")
+        background.aspectFillToSize(fillSize: size)
+        background.zPosition = 0.0
+        background.name = BackgroundNodeName
+        addChild(background)
+        
+        let baseSize = CGSize(width: size.width/2.0, height: size.height/2.0)
         let base = SKSpriteNode(imageNamed: "churnBase.png")
-        
         base.position = CGPoint(x: size.width/2.0, y: size.height/4.0)
-        
-        base.size = CGSize(width: size.width/2.0, height: 91.50)
-        
-        base.zPosition = 100.0
-        
+        //base.size = CGSize(width: size.width/2.0, height: 91.50)
+        base.aspectFillToSize(fillSize: baseSize)
+        base.zPosition = 2.0
         base.name = ChurnBaseNodeName
-        
         addChild(base)
         
+        //let staffSize = CGSize(width: ((size.width/2.0)/11.85), height: size.height/2.0)
         let staff = SKSpriteNode(imageNamed: "churnStaff.png")
-        
         staff.position = CGPoint(x: size.width/2.0, y: size.height/2.0)
-        
-        staff.size = CGSize(width: 4.00, height: 117.30)
-        
+        staff.size = CGSize(width: base.size.width/10.0, height: base.size.height*1.5)
+        //staff.aspectFillToSize(fillSize: staffSize)
+        staff.zPosition = 1.0
         staff.name = ChurnStaffNodeName
-        
         addChild(staff)
         
     }
@@ -45,4 +51,22 @@ class MainGameScene: SKScene {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+extension SKSpriteNode {
+    
+    func aspectFillToSize(fillSize: CGSize) {
+        
+        if texture != nil {
+            self.size = texture!.size()
+            
+            let verticalRatio = fillSize.height / self.texture!.size().height
+            let horizontalRatio = fillSize.width /  self.texture!.size().width
+            
+            let scaleRatio = horizontalRatio > verticalRatio ? horizontalRatio : verticalRatio
+            
+            self.setScale(scaleRatio)
+        }
+    }
+    
 }
