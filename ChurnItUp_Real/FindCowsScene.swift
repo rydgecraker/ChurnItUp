4f490cd549885fdca23b56f19a105df6b636e371
BenRohland <benrohland@yahoo.com>
2018-02-20 14:09:06 -0600

    Getting ready for compass stuff

---
+++ ChurnItUp_Real/FindCowsScene.swift
@@ -0,0 +1,16 @@
+//
+//  FindCowsScene.swift
+//  ChurnItUp_Real
+//
+//  Created by Ben Rohland on 2/20/18.
+//  Copyright © 2018 Craker, Rydge. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class FindCowsScene: SKScene {
+    
+    private let CompassNodeName = "compass"
+    
+}
