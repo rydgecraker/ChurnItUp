2ba7f5be975c8fab14bcf50938c4b8af44d85d33
Jon Vollmer <jvollmer@jons-mbp.cvtc.local>
2018-02-20 15:41:12 -0600

    added player class, and info screen

---
+++ ChurnItUp_Real/PlayerClass.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerClass.swift
+//  ChurnItUp_Real
+//
+//  Created by Jon Vollmer on 2/20/18.
+//  Copyright © 2018 Craker, Rydge. All rights reserved.
+//
+
+import UIKit
+
+class PlayerClass: NSObject {
+    
+}
